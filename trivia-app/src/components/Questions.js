300135a2e3f423ff43aef57601923124f56acf7e
sarah-wines <sarah.lodico.wines@gmail.com>
2021-09-09 15:57:19 -0400

    decodes data

--- trivia-app/src/components/Questions.js
+++ trivia-app/src/components/Questions.js
@@ -1,14 +1,23 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import he from 'he';
 
 export const Questions = ({ id }) => {
+  const [results, setResults] = useState([]);
   useEffect(() => {
     console.log('it runs');
     axios
       .get(`https://opentdb.com/api.php?amount=10&category=${id}`)
       .then((response) => {
+        setResults(response.data.results);
         console.log(response.data.results[3].question);
       });
   }, [id]);
-  return <p>{id}</p>;
+  return (
+    <p>
+      {results.map((result) => {
+        return <p>{he.decode(result.question)}</p>;
+      })}
+    </p>
+  );
 };
